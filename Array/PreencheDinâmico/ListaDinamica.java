87be83a46bee1e8cd9a1f1e3d8a271de6ee7fd7b
Hugo Antunes <86171824+HugoAntt@users.noreply.github.com>
2022-08-25 15:40:00 -0300

    Lista Dinâmica e formas de manipula-la
    Problemas resolvidos

--- Array/PreencheDinâmico/ListaDinamica.java
+++ Array/PreencheDinâmico/ListaDinamica.java
@@ -6,17 +6,37 @@ import java.util.List;
 public class ListaDinamica {
     public static void main(String[] args) {
 
-        List<String> names = new ArrayList<>();
-
-        names.add("Nome1");
-        names.add("Nome2");
-        names.add("Nome3");
-        names.add("Nome4");
+        List<String> names = new ArrayList<>(); 
+        names.add("Nome 1");
+        names.add("Nome 2");
+        names.add("Nome 3");
+        names.add("Nome 4");
+        names.add("Nome 5");
 
         for (int i = 0; i < names.size(); i++) {
             System.out.println(names.get(i));
-
         }
+
+        //Encontrar o indice através do elemento
+        int index = names.indexOf("Nome 5");
+        System.out.println("Posição de 'Nome 5': "+index);
+        //ou
+        System.out.println("Posição de 'Nome 2': "+names.indexOf("Nome 2"));
+
+        //Verifica se a lista está vazia
+        System.out.println("A lista está vazia? "+names.isEmpty());
+
+        //Verifica se a lista contém algum elemento
+        System.out.println("Tem algum elemento nesse espaço? "+names.contains("Nome 1"));
+
+        //Verifica o tamanho da lista
+        System.out.println("Tamanho atual da lista: "+names.size());
+
+        //Remover todos os elementos da lista
+        names.clear();
+
+        //Nova verificação se a lista está vazia
+        System.out.println("A lista está vazia? "+names.isEmpty());
 
     }
 }
