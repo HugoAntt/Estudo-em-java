d01173d986000d404a8065d24070cbaaf5ec1f48
Hugo Antunes <86171824+HugoAntt@users.noreply.github.com>
2022-08-29 14:19:51 -0300

    Modificações

--- Array/Ordenacao/ArraySort/ArraySort.java
+++ Array/Ordenacao/ArraySort/ArraySort.java
@@ -1,4 +1,4 @@
-package Array.Ordenacao;
+package Array.Ordenacao.ArraySort;
 
 import java.util.Scanner;
 import java.util.Arrays;
---
+++ Array/Ordenacao/ForEach/ForEach.java
@@ -0,0 +1,8 @@
+package Array.Ordenacao.ForEach;
+
+public class ForEach {
+    public static void main(String[] args) {
+        
+    }
+    
+}
